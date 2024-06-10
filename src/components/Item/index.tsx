f312b5820d477a86c19db28b97bd0993d51d44ef
Nicholas Jiunchetti <nickjiunchetti@gmail.com>
2024-06-10 17:59:01 -0300

    added item component

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,3 +1,25 @@
-export default function Item() {
-	return <p>item</p>
+import Image from 'next/image'
+
+export default function Item({ featured }: { featured?: boolean }) {
+  const containerClass = featured && 'bg-white rounded border border-gray-100'
+
+  return (
+    <div className={containerClass + ' flex p-4 w-full md:w-1/2 gap-4'}>
+      <Image
+        className='w-1/3'
+        src='/next.svg'
+        alt='Next.js Logo'
+        width={180}
+        height={37}
+        priority
+      />
+      <div className='flex flex-col gap-1'>
+        <p className='font-semibold'>Item Name</p>
+        <p className='text-sm'>
+          Short description of the item goes nicely here.
+        </p>
+        <p className='text-xs text-gray-300'>06/27/2024</p>
+      </div>
+    </div>
+  )
 }
