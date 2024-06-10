3b45559cee951300ba1fd8cd1ede71a9830f3f16
Nicholas Jiunchetti <nickjiunchetti@gmail.com>
2024-06-10 19:24:28 -0300

    added clear search button

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import { itensDataType } from '@/app/page'
 import { useEffect, useState } from 'react'
 
@@ -29,6 +28,12 @@ export default function SearchInput({
       <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
         <span className='material-symbols-outlined text-gray-400'>search</span>
       </div>
+      <button
+        onClick={() => setSearchValue('')}
+        className='absolute inset-y-0 right-0 flex items-center pr-3'
+      >
+        <span className='material-symbols-outlined text-gray-400'>clear</span>
+      </button>
     </div>
   )
 }
